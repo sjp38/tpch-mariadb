395319363143075ec802e6039cac40dedf7ef753
SeongJae Park <sj38.park@gmail.com>
2017-09-09 04:40:11 +0900

    mkqueries: Support scale factor option
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- mkqueries.sh
+++ mkqueries.sh
@@ -4,9 +4,16 @@ BINDIR=`dirname $0`
 
 pushd $BINDIR/tpch-kit/dbgen/queries/
 
+SF=1
+
+if [ $# -eq 1 ]
+then
+	SF=$1
+fi
+
 for i in {1..22}
 do
-	../qgen $i > query-`printf %02d $i`.sql
+	../qgen -s $SF $i > query-`printf %02d $i`.sql
 done
 
 popd
