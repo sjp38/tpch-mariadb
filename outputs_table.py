bd35ce41d8a52f5ea94c11d814c1b41a037c6111
SeongJae Park <sj38.park@gmail.com>
2017-09-12 22:50:02 +0900

    Squash output.sh into outputs_table.py
    outputs.sh and outputs_table.py is too simple to be exists seperately.
    Worse yet, the two scripts just increase confuse.  This commit squash
    output.sh into outputs_table.py and delete outputs.sh script for
    simplicity.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- outputs_table.py
+++ outputs_table.py
@@ -6,7 +6,17 @@ import sys
 sys.path.append(os.environ['HOME'] + '/lazybox/scripts/gen_report')
 import ltldat
 
-text = ""
-for l in sys.stdin:
-    text += l
+if len(sys.argv) < 2:
+    print "Usage: %s <files>" % sys.argv[0]
+    exit(1)
+
+text = "TPCH\n\n\n"
+for path in sys.argv[1:]:
+    text += path + "\n"
+    with open(path, 'r') as f:
+        for line in f:
+            text += line
+    text += "\n\n"
+print text
+
 print ltldat.from_human_readable_txt(text).human_readable_txt()
